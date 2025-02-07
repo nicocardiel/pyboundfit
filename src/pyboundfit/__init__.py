fbac5cf26b12afc630c5d16363958712fb938f81
Nicolás Cardiel <cardiel@ucm.es>
2025-02-07 13:33:29 +0100

    Initial version of the code

---
+++ src/pyboundfit/__init__.py
@@ -0,0 +1,13 @@
+#
+# Copyright 2025 Universidad Complutense de Madrid
+#
+# This file is part of pyboundfit
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+# License-Filename: LICENSE.txt
+#
+
+from .numsplines import AdaptiveLSQUnivariateSpline
+from .version import version
+
+__version__ = version
